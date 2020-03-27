c5748504e9250f74cc0617be23fbe89852db4498
mathiasfc <mathias.falci@gmail.com>
2020-03-26 22:33:39 -0300

    :alien:

--- src/App.js
+++ src/App.js
@@ -1,41 +1,153 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Terminal from "terminal-in-react";
+import { agendaChegada } from "./helper";
 import "./App.css";
-import { numeroAleatorio } from "./helper";
 
 function App() {
   useEffect(() => {
-    console.log("Informe os parâmetros de entrada...");
-    // numeroAleatorio();
+    console.log("Informe os parâmetros de entrada.");
+    console.log("Clique em iniciar para começar a simulação.");
+    console.log("Obs: limite de 1000 número aleatórios");
   }, []);
+
+  const [chegadaMin, setChegadaMin] = useState(2);
+  const [chegadaMax, setChegadaMax] = useState(4);
+
+  const [saidaMin, setSaidaMin] = useState(3);
+  const [saidaMax, setSaidaMax] = useState(5);
+
+  const [servidores, setServidores] = useState(1);
+
+  const [capacidadeDaFila, setCapacidadeDaFila] = useState(5);
+
+  const [chegadaInicial, setChegadaInicial] = useState(3);
+
+  const [nrAleatorios, setNrAleatorios] = useState(1000);
+
+  let terminalRef = null;
+
+  const iniciaSimulacao = () => {
+    terminalRef.runCommandOnActive("clear");
+    const configuracaoInicial = {
+      chegadaMin,
+      chegadaMax,
+      saidaMin,
+      saidaMax,
+      servidores,
+      capacidadeDaFila,
+      nrAleatorios
+    };
+    agendaChegada(chegadaInicial, true, configuracaoInicial);
+  };
+
+  const resetaSimulacao = () => {
+    window.location.reload();
+  };
 
   return (
     <div className="App">
       <div className="Parameters">
         <p>
           <span>Intervalo de Chegada: </span>
-          <input type="number" className="NumberInput" min="0" max="30" /> a
-          <input type="number" className="NumberInput" min="0" max="30" />
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="30"
+            value={chegadaMin}
+            onChange={e => setChegadaMin(e.target.value)}
+          />
+          a
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="30"
+            value={chegadaMax}
+            onChange={e => setChegadaMax(e.target.value)}
+          />
         </p>
         <p>
           <span>Intervalo de Saída: </span>
-          <input type="number" className="NumberInput" min="0" max="30" /> a
-          <input type="number" className="NumberInput" min="0" max="30" />
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="30"
+            value={saidaMin}
+            onChange={e => setSaidaMin(e.target.value)}
+          />
+          a
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="30"
+            value={saidaMax}
+            onChange={e => setSaidaMax(e.target.value)}
+          />
         </p>
         <p>
           <span>Número de servidores: </span>
-          <input type="number" className="NumberInput" min="0" max="30" />
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="30"
+            value={servidores}
+            onChange={e => setServidores(e.target.value)}
+          />
         </p>
         <p>
           <span>Capacidade da fila: </span>
-          <input type="number" className="NumberInput" min="0" max="30" />
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="30"
+            value={capacidadeDaFila}
+            onChange={e => setCapacidadeDaFila(e.target.value)}
+          />
+        </p>
+
+        <p>
+          <span>Chegada Inicial: </span>
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="30"
+            value={chegadaInicial}
+            onChange={e => setChegadaInicial(e.target.value)}
+          />
+        </p>
+
+        <p>
+          <span>Aleatórios utilizados: </span>
+          <input
+            type="number"
+            className="NumberInput"
+            min="0"
+            max="1000"
+            value={nrAleatorios}
+            onChange={e => setNrAleatorios(e.target.value)}
+            style={{ width: "80px" }}
+          />
         </p>
         <p>
-          <button className="button" style={{ backgroundColor: "#e87e7e" }} onClick={() => console.log("HJUE")}>
-            Resetar
-          </button>
-          <button className="button" style={{ backgroundColor: "#7ee892" }}>
+          <button
+            className="button"
+            style={{ backgroundColor: "#7ee892" }}
+            onClick={() => iniciaSimulacao()}
+          >
             Simular
+          </button>
+          <button
+            className="button"
+            style={{ backgroundColor: "#e87e7e" }}
+            onClick={() => resetaSimulacao()}
+          >
+            Reset
           </button>
         </p>
       </div>
@@ -47,6 +159,9 @@ function App() {
         color="white"
         prompt="white"
         allowTabs={false}
+        ref={terminal => {
+          terminalRef = terminal;
+        }}
         commands={{
           "open-google": () => window.open("https://www.google.com/", "_blank")
         }}
--- src/helper/index.js
+++ src/helper/index.js
@@ -1,3 +1,121 @@
-export const numeroAleatorio = () => {
-  console.log("hue");
+export const createRand = seed => {
+  var m = 992;
+  var a = 11;
+  var c = 17;
+
+  var z = seed;
+
+  return function() {
+    z = (a * z + c) % m;
+    return z / m;
+  };
+};
+
+let aleatoriosUtilizados = 0;
+
+const rnd = (min, max) => {
+  aleatoriosUtilizados = aleatoriosUtilizados + 1;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+let config = {};
+//Id para controle de eventos
+let id = 1;
+
+//Escalonador
+let listaDeEventos = [];
+
+//Número de clientes na fila
+let FILA = 0;
+
+//Número de perdas
+let PERDAS = 0;
+
+//Tempo global de execução
+let tempoGlobal = 0;
+
+//Tempo que ocorreu o ultimo evento
+let tempoDoUltimoEvento = 0;
+
+const removeEventoDoEscalonador = evento => {
+  var index = listaDeEventos
+    .map(ev => {
+      return ev.id;
+    })
+    .indexOf(evento.id);
+
+  listaDeEventos.splice(index, 1);
+};
+
+//Executa o próximo passo de acordo com o escalonador
+const proximoPasso = () => {
+  //condição para finalizar a execução
+  if (aleatoriosUtilizados >= config.nrAleatorios) {
+    return;
+  }
+
+  let evento = listaDeEventos.reduce(function(res, obj) {
+    return obj.tempo < res.tempo ? obj : res;
+  });
+
+  removeEventoDoEscalonador(evento);
+
+  console.log(`(${evento.id}) - ${evento.tipo} / Tempo: ${evento.tempo}`);
+  if (evento.tipo === "CHEGADA") {
+    chegada(evento.tempo, tempoDoUltimoEvento);
+  } else if (evento.tipo === "SAIDA") {
+    saida(evento.tempo, tempoDoUltimoEvento);
+  }
+};
+
+export const agendaSaida = tempo => {
+  console.log("agendou uma saída");
+  listaDeEventos.push({ id, tipo: "SAIDA", tempo });
+  id = id + 1;
+};
+
+export const agendaChegada = (
+  tempo,
+  iniciaExecucao = false,
+  configuracaoInicial
+) => {
+  console.log("agendou uma chegada");
+  listaDeEventos.push({ id, tipo: "CHEGADA", tempo });
+  id = id + 1;
+
+  if (iniciaExecucao) {
+    config = configuracaoInicial;
+    proximoPasso();
+  }
+};
+
+export const chegada = (tempoAtual, tempoAnterior) => {
+  tempoGlobal = tempoAtual - tempoAnterior;
+  //armazena tempo do evento corrente para ser usado no próximo como anterior
+  tempoAnterior = tempoAtual;
+
+  if (FILA < config.capacidadeDaFila) {
+    FILA = FILA + 1;
+    if (FILA <= config.servidores) {
+      agendaSaida(tempoGlobal + rnd(config.saidaMin, config.saidaMax));
+    }
+  } else {
+    PERDAS = PERDAS + 1;
+  }
+  agendaChegada(tempoGlobal + rnd(config.chegadaMin, config.chegadaMax));
+  proximoPasso();
+};
+
+const saida = (tempoAtual, tempoAnterior) => {
+  tempoGlobal = tempoAtual - tempoAnterior;
+  //armazena tempo do evento corrente para ser usado no próximo como anterior
+  tempoAnterior = tempoAtual;
+
+  FILA = FILA - 1;
+  if (FILA >= config.servidores) {
+    agendaSaida(tempoGlobal + rnd(config.saidaMin, config.saidaMax));
+  }
+  proximoPasso();
 };
