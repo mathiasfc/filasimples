9d9347f96696d50a5b20178f1e9bcce077d2400e
mathiasfc <mathias.falci@gmail.com>
2020-03-26 22:36:10 -0300

    :rocket: deploy

---
+++ docs/precache-manifest.5d7eb51debca2d5ab95370f8f7d64f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c09820352ecc4b40bb5184474e07269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cd1fe3b93d4165eb3dc",
+    "url": "/static/css/main.f002b5dd.chunk.css"
+  },
+  {
+    "revision": "4b319e5844e956c079ef",
+    "url": "/static/js/2.2de94d70.chunk.js"
+  },
+  {
+    "revision": "8fcb6aaf549782e0d7cb66ea3ac5c5f2",
+    "url": "/static/js/2.2de94d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd1fe3b93d4165eb3dc",
+    "url": "/static/js/main.58e0b374.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
